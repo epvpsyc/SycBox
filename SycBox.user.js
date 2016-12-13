b7d0ccc61d5d5d8a8ae8e23d40ef1c0fb5bee86c
epvpsyc <epvpsyc@users.noreply.github.com>
2016-12-13 10:48:26 +0100

    Initial Commit

---
+++ SycBox.user.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name        ShoutBox
+// @namespace   Elitepvpers
+// @description SB
+// @include     *//www.elitepvpers.com/forum/*
+// @author      Syc
+// @downloadURL https://github.com/epvpsyc/SycBox/raw/master/SycBox.user.js
+// @grant       none
+// ==/UserScript==
