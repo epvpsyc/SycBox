a8066eeaa2fd397df29e520f4c07961c671c6380
epvpsyc <dev@sycophantic.de>
2016-12-16 23:13:19 +0100

    small ui tweaks / code rework

--- SycBox.user.js
+++ SycBox.user.js
@@ -4,20 +4,20 @@
 // @description customized ShoutBox
 // @include     *//www.elitepvpers.com/forum/
 // @author      Syc
-// @version     1.0.0
+// @version     1.0.1
 // @downloadURL https://github.com/epvpsyc/SycBox/raw/master/SycBox.user.js
 // @updateURL   https://github.com/epvpsyc/SycBox/raw/master/SycBox.user.js
 // @grant       none
 // ==/UserScript==
-(function ($)
-{
+/*jshint multistr: true */
+(function ($) {
     // I may or may not feel bad for all of this bad code.
 
-    // refresh will be false for the beginning
+    // refreshActive will be false for the beginning
     // this is supposed to prevent refreshing the SB before the SB is even properly initialized
-    var refresh = false;
+    var refreshActive = false;
 
-    var chat_history = [];
+    var chatHistory = [];
     var settings;
 
     // adding css style this way
@@ -25,15 +25,13 @@
     // and loading it from github
     //
     // thanks to Der-Eddy
-    function appendStyleRaw(style)
-    {
+    function appendStyleRaw(style) {
         if (style) {
             $('<style type="text/css">' + style + '</style>').appendTo('head');
         }
     }
 
-    function updateChatHistory(changes)
-    {
+    function updateChatHistory(changes) {
         // 'changes' will indicate if there is a new message that needs to be added to the SB
 
         $.ajax({
@@ -47,14 +45,11 @@
                 'location': 'full',
                 'first_load': 1,
                 'securitytoken': SECURITYTOKEN,
-                's': ''
             }
-        }).done(function (data)
-        {
+        }).done(function (data) {
             data = $.parseHTML($(data).find('chatbox_content').text());
 
-            $(data).find('tr.alt2').each(function (i)
-            {
+            $(data).find('tr.alt2').each(function (i) {
                 var id = parseInt($(this).find('td:first').attr('id').replace('chat_', ''));
 
                 if (!getMessageById(id)) {
@@ -63,8 +58,6 @@
                     var text = $(this).find('td').last().html().trim();
 
                     text = (settings.showMemes) ? addMemes(text) : text;
-
-                    // color = (username === "Syc") ? 'green' : color;
 
                     var message = {
                         'id': id,
@@ -78,7 +71,7 @@
                         'appended': false,
                     };
 
-                    chat_history.push(message);
+                    chatHistory.push(message);
                     changes = true;
                 }
             });
@@ -87,13 +80,11 @@
                 appendToSB();
             }
 
-            refresh = true;
+            refreshActive = true;
         });
     }
 
-    function initTable()
-    {
-        var input = $('#mgc_cb_evo_input');
+    function initTable() {
         var sbWidth = $('tbody#mgc_cb_evo_opened').width();
         var sbHeight = $('tbody#mgc_cb_evo_opened').height();
 
@@ -103,7 +94,7 @@
         // title
         $(
             '<div class="sycBoxTitle">e*pvp Premium Shoutbox ' +
-            '<span class="thead">[<a target="_blank" href="http://www.elitepvpers.com/forum/mgc_cb_evo.php?do=view_archives&amp;page=1">Archiv</a>]</span>' +
+            '<span class="thead">[<a target="_blank" href="//www.elitepvpers.com/forum/mgc_cb_evo.php?do=view_archives&amp;page=1">Archiv</a>]</span>' +
             '<span class="thead">[<a id="sycBoxMenuBtn" style="text-decoration: underline; cursor: pointer;">SycBox</a>]</span>' +
             '</div>'
         ).insertBefore('#sycBoxTable');
@@ -111,7 +102,7 @@
         // input
         $(
             '<div id="sycBoxInputCon">' +
-            '<form action="http://www.elitepvpers.com/forum/mgc_cb_evo.php" method="post" id="mgc_cb_evo_form" onsubmit="return send_chat()">' +
+            '<form action="//www.elitepvpers.com/forum/mgc_cb_evo.php" method="post" id="mgc_cb_evo_form" onsubmit="return send_chat()">' +
             '<input type="text" id="mgc_cb_evo_input" name="mgc_cb_evo_input" tabindex="1">' +
             '</form>' +
             '</div>'
@@ -125,70 +116,12 @@
         initMenu();
     }
 
-    function initMenu()
-    {
-        var removeSmileysHtml = (settings.removeSmileys) ? 'checked' : '';
-        var showMemesHtml = (settings.showMemes) ? 'checked' : '';
-
-        var memeTableHtml = '<table>';
-        var memes = getMemes();
-
-        var line = '';
-
-        for (i = 0; i < memes.length; i++) {
-            if (i == 0 || memes[i][0] !== memes[i - 1][0]) {
-                line = '<tr><td><img width="16" height="16" src="' + memes[i][0] + '" border="0" class="inlineimg"></td><td>';
-                line += ' '
-            }
-
-            line += memes[i][1];
-
-            if (i === memes.length - 1 || memes[i][0] !== memes[i + 1][0]) {
-                line += '</td></tr>';
-                memeTableHtml += line;
-            } else {
-                line += ' ';
-            }
-        }
-
-        memeTableHtml += '</table>';
-
-        var menuhtml = '<div id="sycBoxMenu">' +
-            '<div class="sycBoxTitle">sycBox Settings' +
-            '<div id="sycBoxMenuClose">x</div>' +
-            '</div>' +
-            '<div id="sycBoxMenuContent">' +
-            '<label>' +
-            '<input type="checkbox" data-sycbox-id="removeSmileys" class="sycBoxSetToggle"' + removeSmileysHtml + '>' +
-            'remove smiley images' +
-            '</label>' +
-            '<br />' +
-            '<label>' +
-            '<input type="checkbox" data-sycbox-id="showMemes" class="sycBoxSetToggle"' + showMemesHtml + '>' +
-            'show custom memes' +
-            '</label>' +
-            '<br /><br />' +
-            memeTableHtml +
-            '</div>' +
-            '<div class="sycBoxMenuFooter" style="left: 0;">' +
-            '<a target="_blank" href="https://github.com/epvpsyc/SycBox/raw/master/SycBox.user.js">Update</a>' +
-            '</div>' +
-            '<div class="sycBoxMenuFooter" style="right: 0;">' +
-            '<a target="_blank" href="https://github.com/epvpsyc/SycBox">Github</a> | &copy;<a target="_blank" href="http://www.elitepvpers.com/forum/members/3409936-syc.html">Syc</a>' +
-            '</div>' +
-            '</div>';
-
-        $(menuhtml).appendTo('body');
-    }
-
-    function appendToSB()
-    {
+    function appendToSB() {
         var appended = false;
 
-        chat_history.reverse().forEach(function (chat, i)
-        {
+        chatHistory.reverse().forEach(function (chat, i) {
             if (!chat.appended) {
-                chat_history[i].appended = true;
+                chatHistory[i].appended = true;
 
                 // layout:
                 // date | username | message text
@@ -207,7 +140,7 @@
             }
         });
 
-        chat_history.reverse();
+        chatHistory.reverse();
 
         // in the case of a message actually being added to the SB, scroll down
         if (appended) {
@@ -219,30 +152,74 @@
         }
     }
 
-    function addMemes(text)
-    {
+    function initMenu() {
+        var removeSmileysHtml = (settings.removeSmileys) ? 'checked' : '';
+        var showMemesHtml = (settings.showMemes) ? 'checked' : '';
+        var memeTableHtml = '<table>';
         var memes = getMemes();
+        var line;
 
-        for (i = 0; i < memes.length; i++) {
-            var find = memes[i][1];
+        memes.forEach(function (meme, i) {
+            if (i === 0 || meme[0] !== memes[i - 1][0]) {
+                line = '<tr><td><img width="16" height="16" src="' + meme[0] + '" border="0" class="inlineimg"></td><td>';
+                line += ' ';
+            }
+
+            line += meme[1];
+
+            if (i === memes.length - 1 || meme[0] !== memes[i + 1][0]) {
+                line += '</td></tr>';
+                memeTableHtml += line;
+            } else {
+                line += ' ';
+            }
+        });
+
+        memeTableHtml += '</table>';
+
+        $(
+            '<div id="sycBoxMenu">' +
+            '<div class="sycBoxTitle">sycBox Settings' +
+            '<div id="sycBoxMenuClose">x</div>' +
+            '</div>' +
+            '<div id="sycBoxMenuContent">' +
+            '<label>' +
+            '<input type="checkbox" data-sycbox-id="removeSmileys" class="sycBoxSetToggle"' + removeSmileysHtml + '>' +
+            'remove smiley images' +
+            '</label>' +
+            '<label>' +
+            '<input type="checkbox" data-sycbox-id="showMemes" class="sycBoxSetToggle"' + showMemesHtml + '>' +
+            'show custom memes' +
+            '</label>' +
+            '<br />' +
+            memeTableHtml +
+            '</div>' +
+            '<div class="sycBoxMenuFooter">' +
+            '<a target="_blank" href="http://www.elitepvpers.com/forum/members/3409936-syc.html">Thread</a> | <a target="_blank" href="https://github.com/epvpsyc/SycBox">Github</a> | &copy;<a target="_blank" href="http://www.elitepvpers.com/forum/members/3409936-syc.html">Syc</a>' +
+            '</div>' +
+            '</div>'
+        ).appendTo('body');
+    }
+
+    function addMemes(text) {
+        getMemes().forEach(function (meme, i) {
+            var find = meme[1];
             var regex = new RegExp(find, 'g');
-
-            var imghtml = '<img width="16" height="16" src="' + memes[i][0] +
-                '" border="0" alt="" title="' + memes[i][2] + '" class="inlineimg">';
+            var imghtml = '<img width="16" height="16" src="' + meme[0] +
+                '" border="0" alt="" title="' + meme[2] + '" class="inlineimg">';
 
             text = text.replace(regex, imghtml);
-        }
+        });
 
         return text;
     }
 
-    function getMemes()
-    {
+    function getMemes() {
         var tfw = 'https://i.imgur.com/DUZLFe6.png';
         var fbm = 'https://i.imgur.com/7PHHNrO.png';
         var fgm = 'https://i.imgur.com/vtttrG2.png';
 
-        var memes = [
+        return [
             [tfw, ':tfw:', 'that feel when'],
             [tfw, ':thatfeelwhen:', 'that feel when'],
             [fbm, ':feelsbadman:', 'feels bad man'],
@@ -253,12 +230,9 @@
             [fgm, ':happyfrog:', 'happy frog'],
             [fgm, ':feelsgoodman:', 'feels good man'],
         ];
-
-        return memes;
     }
 
-    function removeSmileys()
-    {
+    function removeSmileys() {
         var smileys = [
             ['smile.gif', ':)'],
             ['confused.gif', ':confused:'],
@@ -280,20 +254,17 @@
             ['awesome.gif', ':awesome:'],
         ];
 
-        for (i = 0; i < smileys.length; i++) {
-            $("#sycBoxTable img[src$='" + smileys[i][0] + "']").each(function ()
-            {
+        smileys.forEach(function (smiley, i) {
+            $("#sycBoxTable img[src$='" + smileys[i][0] + "']").each(function () {
                 $(this).replaceWith(smileys[i][1]);
             });
-        }
+        });
     }
 
-    function getMessageById(id)
-    {
+    function getMessageById(id) {
         var message;
 
-        chat_history.forEach(function (chat)
-        {
+        chatHistory.forEach(function (chat) {
             if (chat.id === id) {
                 message = chat;
             }
@@ -302,8 +273,7 @@
         return message;
     }
 
-    function getStorage(key, value)
-    {
+    function getStorage(key, value) {
         var prefix = "sycBox_";
 
         var storage = (localStorage.getItem(prefix + key));
@@ -320,15 +290,13 @@
         return storage;
     }
 
-    function setStorage(key, value)
-    {
+    function setStorage(key, value) {
         var prefix = "sycBox_";
         localStorage.setItem(prefix + key, value);
         return value;
     }
 
-    function updateSettings()
-    {
+    function updateSettings() {
         settings = {
             'removeSmileys': getStorage('removeSmileys'),
             'showMemes': getStorage('showMemes'),
@@ -338,38 +306,26 @@
     updateSettings();
     initTable();
 
-    $('#mgc_cb_evo_input').on('submit', function ()
-    {
-        refresh = false;
+    $('#mgc_cb_evo_input').on('submit', function () {
+        refreshActive = false;
         updateChatHistory();
     });
 
-    $('#sycBoxMenuBtn').on('click', function ()
-    {
+    $('#sycBoxMenuBtn').on('click', function () {
         $('#sycBoxMenu').toggle();
     });
 
-    $('#sycBoxMenuClose').on('click', function ()
-    {
+    $('#sycBoxMenuClose').on('click', function () {
         $('#sycBoxMenu').toggle();
     });
 
-    $('input.sycBoxSetToggle').on('change', function ()
-    {
+    $('input.sycBoxSetToggle').on('change', function () {
         setStorage($(this).attr('data-sycbox-id'), $(this).is(':checked'));
         updateSettings();
     });
 
-    $('#sycBoxTbody').on('click', 'span.sycBoxTime', function ()
-    {
+    $('#sycBoxTbody').on('click', 'span.sycBoxTime', function () {
         var message = getMessageById(parseInt($(this).attr('data-sycbox-id')));
-
-        // old: @hh:mm User: 
-        // var bburl = '@' + message.time +
-        //     ' [URL="' + message.user.url + '"]' +
-        //     message.user.name + '[/URL]: ';
-
-        // new: @User: 
         var bburl = '@[URL="' + message.user.url + '"]' +
             message.user.name + '[/URL] ';
 
@@ -377,14 +333,12 @@
         $('#mgc_cb_evo_input').focus();
     });
 
-    window.setInterval(function ()
-    {
+    window.setInterval(function () {
         window.clearTimeout(idleTimeout);
     }, 30000);
 
-    window.setInterval(function ()
-    {
-        if (refresh) {
+    window.setInterval(function () {
+        if (refreshActive) {
             updateChatHistory();
         }
     }, 5000);
@@ -392,10 +346,10 @@
     // apply css
     appendStyleRaw(
         '.sycBoxTitle { \
+            padding: 4px; \
             color: white; \
             background: #1c1e20; \
             text-align: center; \
-            padding: 4px; \
             font: bold 11px tahoma,verdana,geneva,lucida,arial,helvetica,sans-serif; \
         } \
         #sycBoxTable { \
@@ -408,18 +362,14 @@
             table-layout: fixed; \
             overflow: auto; \
         } \
-        /*#sycBoxTable::-webkit-scrollbar, #sycBoxTbody::-webkit-scrollbar { \
-            display: none; \
-        } */\
         #sycBoxTbody { \
             width: 100%; \
         } \
         #sycBoxTbody td { \
-            font-size: 11px; \
-            padding: 1px; \
-            padding-right: 2px; \
+            padding: 1px 2px 1px 2px; \
             vertical-align: top; \
             white-space: nowrap; \
+            font-size: 11px; \
         } \
         #sycBoxTbody tr td:last-child { \
             width: 100%; \
@@ -427,13 +377,12 @@
             white-space: normal; \
         } \
         #mgc_cb_evo_input { \
+            height: 15px; \
             width: 100%; \
             margin-right: -3px; \
+            padding: 0 2px 0 2px; \
             border: 1px solid #CCCCCC; \
             border-top: 0; \
-            height: 15px; \
-            padding-left: 2px; \
-            padding-right: 2px; \
             font-size: 11px; \
             box-sizing: border-box; \
             -moz-box-sizing: border-box; \
@@ -443,34 +392,49 @@
         } \
         #sycBoxMenu { \
             position: absolute; \
-            display: none; \
+            top: 20%; \
+            left: 50%; \
             min-height: 50px; \
             width: 350px; \
+            margin-left: -175px; \
+            display: none; \
             border: 1px solid #CCCCCC; \
             background-color: #EDEDED; \
-            left: 50%; \
-            top: 20%; \
-            margin-left: -175px; \
         } \
         #sycBoxMenuContent { \
-            padding: 3px; \
-            padding-bottom: 20px; \
+            padding: 3px 3px 15px 3px; \
+        } \
+        #sycBoxMenuContent > label { \
+            display: block; \
+            padding: 0 0 3px 4px; \
+        } \
+        input[type="checkbox"].sycBoxSetToggle { \
+            position: relative; \
+            top: -1px; \
+            width: 13px; \
+            height: 13px; \
+            margin:0; \
+            margin-right: 3px; \
+            padding: 0; \
+            vertical-align: bottom; \
+            *overflow: hidden; \
         } \
         #sycBoxMenuClose { \
-            cursor: pointer; \
             position: absolute; \
-            right: 0; \
             top: 0; \
-            padding-right: 4px; \
-            padding-top: 1px; \
+            right: 0; \
+            padding: 1px 4px 0 0; \
             color: white; \
             font-size: 14px; \
+            cursor: pointer; \
         } \
         .sycBoxMenuFooter { \
             position: absolute; \
-            bottom: 0; \
+            left: 0; \
             right: 0; \
+            bottom: 0; \
             padding: 2px; \
+            text-align: right; \
             font-size: 10px; \
         } \
         a:link.sycBox { \
